0a71888392f152047eb80cfea19f9f7d47db6281
kunal-1207 <kunalwaghmare1207@gmail.com>
2024-09-03 23:32:50 +0530

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 import sys
 
+
 def main():
     if len(sys.argv) < 3:
         print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
         exit(1)
 
+
     command = sys.argv[1]
     filename = sys.argv[2]
+
 
     if command != "tokenize":
         print(f"Unknown command: {command}", file=sys.stderr)
@@ -14,21 +17,23 @@ def main():
 
     with open(filename) as file:
         file_contents = file.read()
-        for (
-            character
-        ) in file_contents:
-            if character == "(":
+    for (
+        character
+    ) in file_contents:  # This loop should always run, even if file_contents is empty
+        if character == "(":
 
-    for x in file_contents:
-        if x == "(":
-            print("LEFT_PAREN ( null")
-        elif x == ")":
-            print("RIGHT_PAREN ) null")
+            for x in file_contents:
+                if x == "(":
+                    print("LEFT_PAREN ( null")
+                elif character == ")":
+                    elif x == ")":
+                print("RIGHT_PAREN ) null")
         elif x == "{":
             print("LEFT_BRACE { null")
         elif x == "}":
             print("RIGHT_BRACE } null")
     print("EOF  null")  # This should always run to ensure EOF is printed
+
 
 if __name__ == "__main__":
     main()
